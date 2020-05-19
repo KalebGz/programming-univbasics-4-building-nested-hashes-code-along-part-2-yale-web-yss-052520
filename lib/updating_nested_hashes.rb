02f6558ba2b4c57945b5050ed3c62b1233ae63a7
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 07:17:17 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -54,7 +54,11 @@ def add_characters
       }
    }
   
-  epic_tragedy[]
+  epic_tragedy[:additional_characters]
+  epic_tragedy[:additional_characters] = [
+    {name: },
+    {}
+    
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
